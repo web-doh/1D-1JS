c7b7289482e142214973817495951379a328d98f
web-doh <66818228+web-doh@users.noreply.github.com>
2020-08-21 22:11:04 +0900

    Day1: Drum-Kit

---
+++ Day1 _Drum-Kit/main.js
@@ -0,0 +1,25 @@
+'use strict';
+function playSound(e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    if(!audio) return;
+  
+    audio.currentTime = 0;
+    audio.play();
+    addTransition(e);
+    }
+    
+  function addTransition(e){
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if(!key) return;
+    key.classList.add('playing');
+  }
+
+  function removeTransition(e){
+    if (e.propertyName !== 'transform') return;
+    console.log(e.propertyName);
+    this.classList.remove('playing');
+  }
+
+  const keys = document.querySelectorAll('.key');
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  window.addEventListener('keydown',playSound);
